9716ac04c8fdc12f033b0e60c294b8759758e34e
Celso Cardenas <ccardena@tibco.com>
2023-03-22 23:22:42 -0600

    configuración necesaria con ejemplo para que funcione

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+
+import useForm from './customHooks/useForm';
+import Form from './components/Form'
+
+const formConfig= {
+  title: 'Título de formulario',
+  fields: [
+    {
+      inputType: 'input',
+      name: 'email',
+      type: 'email',
+      placeholder: 'Ej: correo@correo.com',
+      label: 'Correo electrónico'      
+    },
+    {
+      inputType: 'input',
+      name: 'name',
+      type: 'text',
+      placeholder: 'Ej: Cecam 3',
+      label: 'Nombre'      
+    },
+    {
+      inputType: 'input',
+      name: 'age',
+      type: 'number',
+      placeholder: '0',
+      label: 'Edad'      
+    },
+    {
+      inputType: 'textarea',
+      name: 'description',
+      placeholder: 'Ej. La mejor descripción del mundo',
+      label: 'Descripción de... algo'      
+    },
+  ]
+}
 
 function App() {
+  const formState = useForm({
+    email: '',
+    name: '',
+    age: 0,
+    description: ''
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form formState={formState} config={formConfig} />
+      <p>{JSON.stringify(formState.state)}</p>
     </div>
-  );
+  )
 }
 
 export default App;
